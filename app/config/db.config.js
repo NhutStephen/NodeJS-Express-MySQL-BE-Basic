d45c7984bbc4ff9256c50f0a5c7a98cd46904e94
Nhut Huynh <huynhminhnhut@gmail.com>
2020-10-01 18:33:15 +0700

    Create Model customer, start to work with CRUD

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "nrj8kueb",
-    DB: "testdb"
-  };
+  HOST: "localhost",
+  USER: "root",
+  PASSWORD: "nrj8kueb",
+  DB: "testdb"
+};
