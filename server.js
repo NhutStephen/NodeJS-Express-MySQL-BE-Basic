10d344d75459cccf8388ccf19b343f97d54878f3
Nhut Huynh <huynhminhnhut@gmail.com>
2020-10-01 18:30:28 +0700

    Init DB connection for project

---
+++ server.js
@@ -0,0 +1,24 @@
+// Calling Express and Body-parser library
+// Express: framework
+// Body-parser: parse object
+const express = require("express");
+const bodyParser = require("body-parser");
+
+// Create an app using this framework
+const app = express();
+
+// Parse requests of content-type: application/json
+app.use(bodyParser.json());
+
+// Parse requests of content-type: application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to first application with NodeJS + Express + MySQl." });
+});
+
+// Set port, listen for requests
+app.listen(3000, () => {
+  console.log("Server is running on port 3000.");
+});
